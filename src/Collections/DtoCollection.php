321e68504533ce21afe6649703e47f9f16e5ab79
xsaven <gms.sw.xsaven@gmail.com>
2025-05-04 02:48:15 +0300

    src/Collections/DtoCollection.php, src/Collections/LazyDtoCollection.php

--- src/Collections/DtoCollection.php
+++ src/Collections/DtoCollection.php
@@ -5,6 +5,14 @@ namespace Bfg\Dto\Collections;
 use Bfg\Dto\Traits\DtoCollectionMethodsTrait;
 use Illuminate\Support\Collection;
 
+/**
+ * @template TKey of array-key
+ *
+ * @template-covariant TValue
+ *
+ * @implements \ArrayAccess<TKey, TValue>
+ * @implements \Illuminate\Support\Enumerable<TKey, TValue>
+ */
 class DtoCollection extends Collection
 {
     use DtoCollectionMethodsTrait;
--- src/Collections/LazyDtoCollection.php
+++ src/Collections/LazyDtoCollection.php
@@ -8,6 +8,13 @@ use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\LazyCollection;
 
+/**
+ * @template TKey of array-key
+ *
+ * @template-covariant TValue
+ *
+ * @implements \Illuminate\Support\Enumerable<TKey, TValue>
+ */
 class LazyDtoCollection extends LazyCollection
 {
     use DtoCollectionMethodsTrait;
