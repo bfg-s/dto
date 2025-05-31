0a2ccec65f0566fd611fb07ff3f99f3db4b428c9
xsaven <gms.sw.xsaven@gmail.com>
2025-06-01 00:52:28 +0300

    src/Console/MakeDtoCommand.php, src/Console/stubs/dto.stub

--- src/Console/MakeDtoCommand.php
+++ src/Console/MakeDtoCommand.php
@@ -2,8 +2,10 @@
 
 namespace Bfg\Dto\Console;
 
+use Bfg\Dto\Collections\DtoCollection;
 use Illuminate\Console\GeneratorCommand;
 use Illuminate\Support\Str;
+use Symfony\Component\Console\Input\InputArgument;
 
 class MakeDtoCommand extends GeneratorCommand
 {
@@ -27,6 +29,14 @@ class MakeDtoCommand extends GeneratorCommand
      * @var string
      */
     protected $type = 'Dto';
+
+    /**
+     * @var array|string[]
+     */
+    protected array $simplePhpTypes = [
+        'int', 'integer', 'float', 'double', 'string', 'bool', 'boolean', 'array', 'object', 'mixed',
+        'null', 'callable', 'iterable', 'resource'
+    ];
 
     /**
      * Get the stub file for the generator.
@@ -65,11 +75,13 @@ class MakeDtoCommand extends GeneratorCommand
         $namespace = $this->getNamespace(
             Str::replaceFirst($this->rootNamespace(), 'App\\Dto\\', $this->qualifyClass($this->getNameInput()))
         );
-
+        $properties = $this->makeProperties();
         $replace = [
             '{{ dtoNamespace }}' => $namespace,
             '{{ dto }}' => $dto,
             '{{dto}}' => $dto,
+            '{{ uses }}' => $properties['uses'],
+            '{{ fields }}' => $properties['properties'],
         ];
 
         return str_replace(
@@ -77,6 +89,61 @@ class MakeDtoCommand extends GeneratorCommand
             array_values($replace),
             parent::buildClass($name)
         );
+    }
+
+    /**
+     * @return array{uses: string, properties: string}
+     */
+    protected function makeProperties(): array
+    {
+        $parameters = [];
+        $uses = [];
+
+        foreach ($this->argument('fields') as $field) {
+            $field = explode(' ', $field);
+            $type = count($field) > 1 ? $field[0] : 'mixed';
+            $name = count($field) > 1 ? $field[1] : $field[0];
+            $type = explode('|', $type);
+            foreach ($type as $typeKey => $unionType) {
+                $unionType = trim($unionType);
+                if (in_array($unionType, $this->simplePhpTypes)) {
+                    continue;
+                }
+                if (! isset($uses[$unionType])) {
+                    $unionTypeName = class_basename($unionType);
+                    if ($unionTypeName === 'Collection' || $unionTypeName === 'DtoCollection') {
+                        $uses[$unionTypeName] = DtoCollection::class;
+                        $type[$typeKey] = 'DtoCollection';
+                    } else {
+                        $dtoClass = "App\\Dto\\$unionType";
+                        if (class_exists($dtoClass)) {
+                            $uses[$unionTypeName] = $dtoClass;
+                            $type[$typeKey] = $unionTypeName;
+                        } else {
+                            $modelClass = "App\\Models\\$unionType";
+                            if (class_exists($modelClass)) {
+                                $uses[$unionTypeName] = $modelClass;
+                                $type[$typeKey] = $unionTypeName;
+                            } else {
+                                $appClass = "App\\$unionType";
+                                if (class_exists($appClass)) {
+                                    $uses[$unionTypeName] = $appClass;
+                                    $type[$typeKey] = $unionTypeName;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            $type = implode('|', $type);
+            $parameters[] = "public $type \$$name,";
+        }
+
+        return [
+            'properties' => $parameters ? (str_repeat(' ', 8)
+                . implode(PHP_EOL . str_repeat(' ', 8), $parameters)) : '',
+            'uses' => $uses ? "\nuse " . implode(';' . PHP_EOL . 'use ', array_values($uses)) . ";" : ''
+        ];
     }
 
     /**
@@ -90,6 +157,19 @@ class MakeDtoCommand extends GeneratorCommand
         $name = (string) Str::of($name)->replaceFirst($this->rootNamespace(), '')->finish('Dto');
 
         return app_path('Dto/'.str_replace('\\', '/', $name).'.php');
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['name', InputArgument::REQUIRED, 'The name of the '.strtolower($this->type)],
+            ['fields', InputArgument::IS_ARRAY, 'The fields of the '.strtolower($this->type)],
+        ];
     }
 
     /**
